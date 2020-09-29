a954fb4a9a8393de312e5854e1a9708280f7ee73
wmj-wfj <3187755914@qq.com>
2020-09-29 14:07:59 +0800

    'qq登陆'

---
+++ src/views/qq.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <button @click="qq">qq登陆</button>
+</div>
+</template>
+
+<script>
+export default {
+  components:{},
+  props:{},
+  data(){
+    return {
+    }
+  },
+methods:{
+    qq(){
+        window.open(
+            'http://www.wwyyyy.shop/Admin/login/Hindex',
+            'newwindow',
+            'height=500,width=500,top=0,left=0,toolbar=no,menubar=no,scrollbars=no,resizable=no,location=no,status=no'
+        )
+    }
+},
+}
+</script>
+<style scoped>
+</style>
