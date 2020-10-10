1a3a476b65a6152146766df54c9b066f09e1bca8
wmj-wfj <3187755914@qq.com>
2020-10-10 15:57:33 +0800

    qq登录

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import input from '@/views/input.vue'
-import avatar from '@/views/avatar.vue'
-import avatar1 from '@/views/avatar1.vue'
-
-import dlog from '@/views/dlog.vue'
-import bank from '@/views/bank.vue'
+import input from '@/views/input.vue' //点击按钮更改input里面的内容演示
+import avatar from '@/views/avatar.vue' //更换头像 （更换为base64码格式）
+import avatar1 from '@/views/avatar1.vue' //更换头像 （转化为blob格式）
+import qqLogin from '@/views/qqLogin.vue' //QQ登录
+import dlog from '@/views/dlog.vue' //点击显示头像
+import bank from '@/views/bank.vue' //下拉刷新
 
 
 Vue.use(Router)
@@ -37,6 +37,11 @@ export default new Router({
             path: '/bank',
             name: 'bank',
             component: bank
+        },
+        {
+            path: '/qqLogin',
+            name: 'qqLogin',
+            component: qqLogin
         },
 
     ]
---
+++ src/views/qqlogin.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- QQ回调 -->
+
+    <!-- 1.先在index.html里面引入js脚手包
+   <script type="text/javascript"  charset="utf-8" src="http://connect.qq.com/qc_jssdk.js" data-appid="QQ登录时获取的AppId" data-redirecturi="申请接入qq登录时输入的回调地址"></script>
+ -->
+ <!-- 2.在页面内插入QQ登录按钮的位置
+ <span id="qqLoginBtn"></span>
+ 在created里面写入
+    //调用QC.Login方法，指定btnId参数将按钮绑定在容器节点中
+   QC.Login({
+       //btnId：插入按钮的节点id，必选
+       btnId:"qqLoginBtn",
+       //用户需要确认的scope授权项，可选，默认all
+       scope:"all",
+       //按钮尺寸，可用值[A_XL| A_L| A_M| A_S|  B_M| B_S| C_S]，可选，默认B_S
+       size: "A_XL"
+   }, function(reqData, opts){//登录成功
+       //根据返回数据，更换按钮显示状态方法
+       var dom = document.getElementById(opts['btnId']),
+       _logoutTemplate=[
+            //头像
+            '<span><img src="{figureurl}" class="{size_key}"/></span>',
+            //昵称
+            '<span>{nickname}</span>',
+            //退出
+            '<span><a href="javascript:QC.Login.signOut();">退出</a></span>'
+       ].join("");
+       dom && (dom.innerHTML = QC.String.format(_logoutTemplate, {
+           nickname : QC.String.escHTML(reqData.nickname), //做xss过滤
+           figureurl : reqData.figureurl
+       }));
+   }, function(opts){//注销成功
+         alert('QQ登录 注销成功');
+   }
+);
+ -->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methoods: {},
+};
+</script>
+<style>
+</style>
